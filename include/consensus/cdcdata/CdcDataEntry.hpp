2d16278e22be0c6eab126b4a248050a83490a968
cdcchain <cdcchain@gmail.com>
2018-03-27 20:54:28 +0800

    generalize cdcdata type

--- include/consensus/cdcdata/CdcDataEntry.hpp
+++ include/consensus/cdcdata/CdcDataEntry.hpp
@@ -15,98 +15,26 @@ namespace cdcchain {
 		//front declaration for typedef
 		class ChainInterface;
 
-		struct CdcDataCondition {
-			fc::enum_type<fc::unsigned_int, CdcDataTypeEnum> cdcdata_type;
-			std::vector<char> cdcdata_data;
-
-			CdcDataCondition() {}
-
-			CdcDataCondition(const CdcDataCondition& cond) : cdcdata_type(cond.cdcdata_type),
-				cdcdata_data(cond.cdcdata_data) {}
-
-			template<typename CdcDataType>
-			CdcDataCondition(const CdcDataType& t)
-			{
-				cdcdata_type = CdcDataType::type;
-				cdcdata_data = fc::raw::pack(t);
-			}
-
-			template<typename CdcDataType>
-			CdcDataType as()const
-			{
-				FC_ASSERT(cdcdata_type == CdcDataType::type, "", ("proposal_type", cdcdata_type)("CdcDataType", CdcDataType::type));
-				return fc::raw::unpack<CdcDataType>(cdcdata_data);
-			}
-		};
-
 		struct CdcDataEntry;
 		typedef fc::optional<CdcDataEntry> oCdcDataEntry;
 
 		struct CdcDataEntry {
 			CdcDataDigestIdType cdcdata_id;
-			CdcDataCondition cdcdata_condition;
+			fc::enum_type<fc::unsigned_int, CdcDataTypeEnum> cdcdata_type;
+			std::string cdcdata_remark;
 			fc::time_point_sec checkin_time;
 			Address checkin_admin;
 
 			CdcDataEntry() {}
-			CdcDataEntry(const CdcDataDigestIdType& _data_id, const CdcDataCondition& cond, 
+
+			CdcDataEntry(const CdcDataDigestIdType& _data_id, const CdcDataTypeEnum& _cdcdata_type, const string& _cdcdata_remark,
 				const fc::time_point_sec& _checkin_time, const Address& admin)
-				:cdcdata_id(_data_id), cdcdata_condition(cond), checkin_time(_checkin_time), checkin_admin(admin) {}
+				:cdcdata_id(_data_id), cdcdata_type(_cdcdata_type), cdcdata_remark(_cdcdata_remark), 
+				checkin_time(_checkin_time), checkin_admin(admin) {}
 
 			static oCdcDataEntry lookup(const ChainInterface&, const CdcDataDigestIdType&);
 			static void store(ChainInterface&, const CdcDataDigestIdType&, const CdcDataEntry&);
 			static void remove(ChainInterface&, const CdcDataDigestIdType&);
-		};
-
-		struct UserReceiptData {
-			static const CdcDataTypeEnum type;
-
-			UserReceiptData() {}
-			UserReceiptData(const Address& _chunk_from)
-				:data_chunk_from(_chunk_from) {}
-
-			Address data_chunk_from;
-		};
-
-		struct MiningResultData {
-			static const CdcDataTypeEnum type;
-
-			MiningResultData() {}
-			MiningResultData(const Address& _merchant_id, 
-				const TransactionIdType& _task_id, const Address& _result_from)
-				:from_merchant_id(_merchant_id), mining_task_id(_task_id), mining_result_from(_result_from) {}
-
-			Address from_merchant_id;
-			TransactionIdType mining_task_id;
-			Address mining_result_from;
-		};
-		
-		struct AdPuttingFeedbackData {
-			static const CdcDataTypeEnum type;
-
-			AdPuttingFeedbackData() {}
-			AdPuttingFeedbackData(const Address& _merchant_id, 
-				const TransactionIdType& _task_id, const Address& _feeback_from)
-				:from_merchant_id(_merchant_id), mining_task_id(_task_id),  putting_feeback_from(_feeback_from) {}
-
-			Address from_merchant_id;
-			TransactionIdType mining_task_id;
-			Address putting_feeback_from;
-		};
-
-		struct AdPuttingResultData {
-			static const CdcDataTypeEnum type;
-
-			AdPuttingResultData() {}
-			AdPuttingResultData(const Address& _merchant_id, const TransactionIdType& _task_id,
-				const CdcDataDigestIdType& _feedback_id, const Address& _result_from)
-				:from_merchant_id(_merchant_id), mining_task_id(_task_id), 
-				putting_feedback_id(_feedback_id), putting_result_from(_result_from) {}
-
-			Address from_merchant_id;
-			TransactionIdType mining_task_id;
-			CdcDataDigestIdType putting_feedback_id;
-			Address putting_result_from;
 		};
 
 		class CdcDataDbInterface
@@ -120,7 +48,6 @@ namespace cdcchain {
 	}
 }
 
-
 FC_REFLECT_ENUM(cdcchain::consensus::CdcDataTypeEnum,
 (user_receipt_data)
 (mining_result_data)
@@ -128,37 +55,10 @@ FC_REFLECT_ENUM(cdcchain::consensus::CdcDataTypeEnum,
 (ad_putting_result_data)
 )
 
-FC_REFLECT(cdcchain::consensus::CdcDataCondition,
-(cdcdata_type)
-(cdcdata_data)
-)
-
 FC_REFLECT(cdcchain::consensus::CdcDataEntry,
 (cdcdata_id)
-(cdcdata_condition)
+(cdcdata_type)
+(cdcdata_remark)
 (checkin_time)
 (checkin_admin)
-)
-
-FC_REFLECT(cdcchain::consensus::UserReceiptData,
-(data_chunk_from)
-)
-
-FC_REFLECT(cdcchain::consensus::MiningResultData,
-(from_merchant_id)
-(mining_task_id)
-(mining_result_from)
-)
-
-FC_REFLECT(cdcchain::consensus::AdPuttingFeedbackData,
-(from_merchant_id)
-(mining_task_id)
-(putting_feeback_from)
-)
-
-FC_REFLECT(cdcchain::consensus::AdPuttingResultData,
-(from_merchant_id)
-(mining_task_id)
-(putting_feedback_id)
-(putting_result_from)
 )
--- src/consensus/cdcdata/CdcDataEntry.cpp
+++ src/consensus/cdcdata/CdcDataEntry.cpp
@@ -5,11 +5,6 @@
 namespace cdcchain {
 	namespace consensus {
 
-		const CdcDataTypeEnum UserReceiptData::type = user_receipt_data;
-		const CdcDataTypeEnum MiningResultData::type = mining_result_data;
-		const CdcDataTypeEnum AdPuttingFeedbackData::type = ad_putting_feedback_data;
-		const CdcDataTypeEnum AdPuttingResultData::type = ad_putting_result_data;
-
 		oCdcDataEntry CdcDataEntry::lookup(const ChainInterface& db, const CdcDataDigestIdType& id)
 		{
 			try
